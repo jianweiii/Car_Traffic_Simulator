1b39b780c214a889f6ec7730858381d5b5bf73ae
jianwei <huang_jianwei1@hotmail.com>
2019-12-14 17:49:29 +0800

    Add moveVehicle() in Road() class
    able to move vehicle object in road class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,18 +1,24 @@
 package com.company;
 
+import java.lang.reflect.Array;
+
 public class Main {
 
     public static void main(String[] args) {
 	    Road road1 = new Road();
+        Road road2 = new Road();
+
+
 	    road1.setVehicle(new Vehicle("Car"));
-	    Vehicle vehicle1 = road1.getVehicle();
 
-	    Road road2 = new Road();
-	    road2.setVehicle(vehicle1);
-	    String type = road2.getVehicle().getType();
-        System.out.println(type);
-
-        System.out.println(road1.getVehicle());
+//	    Vehicle vehicle1 = road1.getVehicle();
+//
+//	    Road road2 = new Road();
+//	    road2.setVehicle(vehicle1);
+//	    String type = road2.getVehicle().getType();
+//        System.out.println(type);
+//
+//        System.out.println(road1.getVehicle());
 
 
 
--- src/com/company/Road.java
+++ src/com/company/Road.java
@@ -1,11 +1,14 @@
 package com.company;
 
+import java.util.Random;
+
 public class Road {
     private Vehicle vehicle;
-    private int[][] roadArray;
+    public Vehicle[][] roadArray;
 
     public Road() {
-        
+        // Initiates number of road segments
+        roadArray = new Vehicle[1][randomRoadLength()];
     }
 
     public Vehicle getVehicle() {
@@ -16,6 +19,38 @@ public class Road {
     }
 
     public void setVehicle(Vehicle vehicle) {
-        this.vehicle = vehicle;
+        this.roadArray[0][0] = vehicle;
+    }
+
+    public void displayRoad() {
+        for (Vehicle[] vehicleArray: roadArray) {
+            for (Vehicle vehicle: vehicleArray) {
+                if (vehicle != null) {
+                    System.out.print(vehicle.getType() + " | ");
+                } else {
+                    System.out.print("_ | ");
+                }
+
+            }
+        }
+    }
+
+    /*
+    Randoms the length of each road
+     */
+    private int randomRoadLength() {
+        return (new Random().nextInt(15-6+1) + 6);
+    }
+
+    /*
+    Move Vehicle 1 slot up
+     */
+    public void moveVehicle() {
+        System.out.println();
+        for (int i=(roadArray[0].length-1);i>0;i--) {
+            roadArray[0][i] = roadArray[0][i-1];
+            roadArray[0][i-1] = null;
+
+        }
     }
 }
